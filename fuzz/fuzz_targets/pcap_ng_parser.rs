98a04a276d5d746f08b7241dd10271d0700f6e32
courvoif <courvoif@pm.me>
2022-09-19 22:01:19 +0200

    fix: fuzzing

--- fuzz/fuzz_targets/pcap_ng_parser.rs
+++ fuzz/fuzz_targets/pcap_ng_parser.rs
@@ -3,11 +3,11 @@ use libfuzzer_sys::fuzz_target;
 use pcap_file::PcapNgParser;
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok((rem, pcap_parser)) = PcapNgParser::new(data) {
+    if let Ok((rem, mut pcapng_parser)) = PcapNgParser::new(data) {
         let mut src = rem;
 
         while !src.is_empty() {
-            let _ = pcap_parser.next_packet(src);
+            let _ = pcapng_parser.next_block(src);
             src = &src[1..];
         }
     }
--- fuzz/fuzz_targets/pcap_ng_reader.rs
+++ fuzz/fuzz_targets/pcap_ng_reader.rs
@@ -3,11 +3,7 @@ use libfuzzer_sys::fuzz_target;
 use pcap_file::PcapNgReader;
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok(pcap_reader) = PcapNgReader::new(data) {
-        for maybe_block in pcap_reader {
-            if let Ok(block) = maybe_block {
-                let _ = block.parsed();
-            }
-        }
+    if let Ok(mut pcapng_reader) = PcapNgReader::new(data) {
+        while let Some(_block) = pcapng_reader.next_block() {}
     }
 });
