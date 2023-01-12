1cbf629ba69d9de019b5909670bfee35a2b1d55b
courvoif <courvoif@pm.me>
2023-01-12 17:03:41 +0100

    Update Readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
 #![allow(clippy::unreadable_literal)]
 #![deny(missing_docs)]
 
-//! Provides parsers, readers and writers for Pcap and PcapNg files.
-//!
-//! For Pcap files see
-//! [PcapParser](struct.PcapParser.html), [PcapReader](struct.PcapReader.html) and [PcapWriter](struct.PcapWriter.html).
-//!
-//! For PcapNg files see
-//! [PcapNgParser](struct.PcapNgParser.html), [PcapNgReader](struct.PcapNgReader.html) and [PcapNgWriter](struct.PcapNgWriter.html).
-
+#![doc = include_str!("../README.md")]
 
 pub use common::*;
 pub use errors::*;
